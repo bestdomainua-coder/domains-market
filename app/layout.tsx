e4daeeb5b15870c957f453666d2a0f25970e3764
bestdomainua-coder <bestdomainua@gmail.com>
2026-03-26 10:29:18 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Domains Market",
+  description: "Купівля доменів",
+};
+
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="uk">
+      <body>{children}</body>
+    </html>
+  );
+}
